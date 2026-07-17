b142f4998a4690b0c926c4e9ee2798ee0569caca
Ahmed E. <2152369@bdeb.qc.ca>
2026-07-17 15:48:48 -0400

    Correction deploiement vercel

--- src/app/contact/api/route.js
+++ src/app/contact/api/route.js
@@ -1,10 +1,8 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-// On initialise l'instance resend avec ta clé secrète
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const body = await request.json();
     const { name, email, message } = body;
